1c15fe59fff27fab4854a6f5e504696f70409450
Magdalena Majchrzak <mmajchrzak@pl.sii.eu>
2021-02-07 14:25:51 +0100

    fix test

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,74 @@
 import express, { NextFunction, Request, Response } from "express";
 
-export interface Book {
-    id: number,
-    authors: string[],
-    title: string,
-}
+const app: express.Application = express();
+
+type Book = {
+    id: number;
+    authors: string[];
+    title: string;
+};
 
 let books: Book[] = [
     {
         id: 1,
-        authors: ['Jonathan Haidt'],
-        title: 'Coddling of the American Mind',
+        authors: ["Jonathan Haidt"],
+        title: "Coddling of the American Mind",
     },
     {
         id: 2,
-        authors: ['Dan Heath', 'Chip Heath'],
-        title: 'Switch: How to change when change is hard',
+        authors: ["Dan Heath", "Chip Heath"],
+        title: "Switch: How to change when change is hard",
     },
-    {
-        id: 3,
-        authors: ['Kathy Sierra'],
-        title: 'Badass: Making users awesome'
-    },
+    { id: 3, authors: ["Kathy Sierra"], title: "Badass: Making users awesome" },
     {
         id: 4,
-        authors: ['Daniel Kahneman'],
-        title: 'Thinking fast, thinking slow',
+        authors: ["Daniel Kahneman"],
+        title: "Thinking fast, thinking slow",
     },
-    {
-        id: 5,
-        authors: ['Caroline Dweck'],
-        title: 'Mindset'
-    },
-    {
-        id: 6,
-        authors: ['Michael Walker'],
-        title: 'Why we sleep?'
-    },
+    { id: 5, authors: ["Caroline Dweck"], title: "Mindset" },
+    { id: 6, authors: ["Michael Walker"], title: "Why we sleep?" },
 ];
-
-const app = express();
-const PORT = 3001;
 
 app.use(express.json());
 
-
-app.use((req: Request, res: Response,  next: NextFunction) => {
-    // console.log('Time:', Date.now())
-    // next()
-
+app.get("/", (req: Request, res: Response) => {
+    const name = "Wojtek";
+    res.send(`Hello ${name}!`);
 });
 
-app.get('/books', function (req, res) {
-    res.json(books);
+app.get("/books", (req: Request, res: Response) => {
+    res.status(200).json(books);
 });
 
-app.post('/books', function (req, res) {
-    const book: Book = {
-        id: books.length + 1,
-        authors: req.body.authors,
-        title: req.body.title
-    }
-    books.push(book);
+app.post("/books", (req: Request, res: Response) => {
+    const body = req.body;
+
+    books.push(body);
+
     res.sendStatus(201);
 });
 
-app.get('/books/:id', function (req, res) {
-    let find = books.find(book => book.id === parseInt(req.params.id));
-    res.json(find);
+app.get("/books/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    res.status(200).json(books.filter((book) => book.id === id));
 });
 
-app.put('/books/:id', (req, res) => {
-    let index = books.findIndex(book => book.id === parseInt(req.params.id));
-    books[index] = {
-        id: books[index].id,
-        authors: req.body.authors,
-        title: req.body.title
-    }
+app.put("/books/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+
+    books = books.map((book) => (book.id === id ? body : book));
+
     res.sendStatus(202);
 });
 
-app.delete('/books/:id', function (req, res) {
-    const idx = books.findIndex(element => element.id === Number(req.params.id));
-    books.splice(idx, 1);
-    res.sendStatus(201);
-});
+app.delete("/books/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+    books = books.filter((book) => book.id !== id);
+
+    res.sendStatus(202);
 });
 
 export default app;
